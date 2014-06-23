9958fe0c4ba908b9a237bbd603f914b9ddace42d
harrypan <gs0622@gmail.com>
2014-06-23 12:50:33 +0800

    add OddEvenSort

--- bubblesort.c
+++ bubblesort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <assert.h>
 int *create_array(int n)
 {
 	int i, *p;
@@ -13,22 +13,38 @@ int *create_array(int n)
 	}
 	return p;
 }
-
 void print_array(int *p, int n)
 {
 	for (;p&&n>0;n--,p++) {
 		printf("%.2d%s", *p, ((n-1)>0)? " ":"\n");
 	}
 }
-
 void swap(int *p, int *q)
 {
-	int tmp = *p;
+	int tmp;
+    assert(p&&q);
+    tmp = *p;
 	*p = *q;
 	*q = tmp;
 }
-
-int bubblesort(int *p, int n)
+/*shuffle with O(n) time complexity*/
+void shuffle(int a[], int n)
+{
+    int i, j;
+    srand(time(NULL));
+#if 0
+    for (i=0;i<n-1;i++) {
+        j = i+rand()/(RAND_MAX/(n-i)+1);
+        swap(&a[i], &a[j]);
+    }
+#else
+    for (i=n-1;i>0;i--) {
+        j = rand()%(i+1);
+        swap(&a[i], &a[j]);
+    }
+#endif
+}
+void bubblesort(int *p, int n)
 {
 	int i, j;
 	for (i=n-1; i>0; i--)
@@ -37,14 +53,29 @@ int bubblesort(int *p, int n)
 			//if (p[j]<p[j+1]) swap(&p[j], &p[j+1]); /*descending*/
 		}
 }
-
+void OddEvenSort(int a[], int n)
+{
+    int i, sorted;
+    while (1) {
+        sorted = 1; /*true*/
+        for (i=1;i<n-1;i+=2)
+            if (a[i]>a[i+1]) swap(&a[i], &a[i+1]), sorted = 0;
+        for (i=0;i<n-1;i+=2)
+            if (a[i]>a[i+1]) swap(&a[i], &a[i+1]), sorted = 0;
+        if (sorted) break;
+    }
+}
 int main(int argc, char **argv)
 {
 	int n, *p;
 	n = (argc>1)? atoi(argv[1]) : 10;
 	p = create_array(n);
-	print_array(p, n);
+	//print_array(p, n);
 	bubblesort(p, n);
+	print_array(p, n);
+    shuffle(p, n);
+	//print_array(p, n);
+    OddEvenSort(p, n);
 	print_array(p, n);
 	free(p);
 	return 0;
