74cedc8e41223c17f801f43c54c42ade2fca6b36
harrypan <gs0622@gmail.com>
2014-07-14 02:49:51 +0800

    add matrix transpose

--- matrix.c
+++ matrix.c
@@ -7,7 +7,7 @@ void mprint(int *x, int row, int col)
         for (c=0;c<col;c++)
             printf("%d%s", *x++, c==(col-1)?"\n":" ");
 }
-
+/*Matrix addition*/
 void madd(int *x, int *y, int *z, int row, int col)
 {
     int r, c;
@@ -15,14 +15,28 @@ void madd(int *x, int *y, int *z, int row, int col)
         for (c=0;c<col;c++)
             *z++=(*x++)+(*y++);
 }
+/*Matrix Transpose*/
+void mtranspose(int *x, int *y, int row, int col)
+{
+    int r, c;
+    for (r=0;r<col;r++)
+        for (c=0;c<row;c++)
+            *y++=*(x+((c*col)/*new row*/+r/*new col*/));
+}
+
 
 int main(void)
 {
     int a[2][3]={{1,3,1},{1,0,0}};
     int b[2][3]={{0,0,5},{7,5,0}};
     int c[2][3]={};
+    int d[2][3]={{1,2,3},{0,-6,7}};
+    int e[3][2]={};
     madd((int *)a, (int *)b, (int *)c, 2, 3);
     mprint((int *)c, 2, 3);
+    printf("\n");
+    mtranspose((int *)d, (int *)e, 2, 3);
+    mprint((int *)e, 3, 2);
     return 0;
 }
 
