925c45ec42ce9b41eea0a505caa88d1dcf839e58
harrypan <gs0622@gmail.com>
2014-07-15 00:37:11 +0800

    add matrix multiplication

--- matrix.c
+++ matrix.c
@@ -25,6 +25,19 @@ void madd(int *x, int *y, int *z, int row, int col)
         for (c=0;c<col;c++)
             *z++=(*x++)+(*y++);
 }
+/*Matrix multiplication*/
+void mmul(int *x, int *y, int *z, int n, int m, int p)
+{
+    int r, c, k;
+    for (r=0;r<n;r++) {
+        for (c=0;c<p;c++) {
+            *z=0;
+            for (k=0;k<m;k++)
+                *z+=(*(x+(r*m+k)))*(*(y+(k*p+c)));
+            z++;
+        }
+    }
+}
 /*Matrix Transpose*/
 void mtranspose(int *x, int *y, int row, int col)
 {
@@ -42,13 +55,19 @@ int main(void)
     int c[2][3]={};
     int d[2][3]={{1,2,3},{0,-6,7}};
     int e[3][2]={};
-    int **f=make2darr(2, 3);
-    free(f);
+    int f[2][2]={{1,2},{3,4}};
+    int g[2][2]={{0,1},{0,0}};
+    int h[2][2]={};
+    int **aa=make2darr(2, 3);
+    free(aa);
     madd((int *)a, (int *)b, (int *)c, 2, 3);
     mprint((int *)c, 2, 3);
     printf("\n");
     mtranspose((int *)d, (int *)e, 2, 3);
     mprint((int *)e, 3, 2);
+    printf("\n");
+    mmul((int *)f, (int *)g, (int *)h, 2, 2, 2);
+    mprint((int *)h, 2, 2);
     return 0;
 }
 
