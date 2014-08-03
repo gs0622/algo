5023524bd3c8fd53dd5d381222fe76fd12d71f0d
harrypan <gs0622@gmail.com>
2014-08-04 01:41:11 +0800

    revise loop to skip minor condiction

---
+++ TwoSumLinear.c
@@ -0,0 +1,36 @@
+/*Given an array A[] and a number x, check for pair in A[] with sum as x
+  Example: -8 1 4 6 10 45
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+int cmp(const void *p, const void *q)
+{
+    return (*(int *)p - *(int *)q);
+}
+void TwoSumLinear(int arr[], int sz, int sum)
+{
+    int i, j, k;
+    qsort(arr, sz, sizeof(int), cmp);
+    for (i = 0, j = sz - 1; j > i; ) {
+        k = arr[i] + arr[j];
+        if (k == sum) printf("%d %d\n", arr[i], arr[j]), ++i, --j;
+        else if (k < sum) ++i;
+        else if (k > sum) --j;
+        
+    }
+}
+int main(int argc, char *argv[])
+{
+    int i, n, *arr;
+    const int sum = 16;
+    if (1 == argc) return 0;
+    n = argc - 1;
+    arr = malloc(sizeof(int)*n);
+    assert(arr);
+    for (i = 0; i < n; i++) arr[i] = atoi(argv[i+1]);
+    TwoSumLinear(arr, n, sum);
+    free(arr);
+    return 0;
+}
+
