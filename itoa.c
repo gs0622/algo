fb0b671f689f1d0f2a54d73eeaf27f1b25f513d7
harrypan <gs0622@gmail.com>
2014-08-12 00:04:49 +0800

    refine itoa to process nibble from MSB to LSB

--- itoa.c
+++ itoa.c
@@ -2,27 +2,23 @@
 #include <stdlib.h>
 
 char code[] = {"0123456789abcdef"};
+
 char *itoa(int n)
 {
-    int i = 0, j, k;
-    static char buf[64] = {};
-    //if (n < 0) buf[i++] = '-', neg = 1;
-    while (n) {
-        if (n & ~0xf) buf[i++] = code[n & 0xf];
-        else  buf[i++] = code[n];
-        n = (unsigned)n >> 4; 
-    }
-    for (j = i-1; j > (i-1)/2; j--) k = buf[j], buf[j] = buf[i-1-j], buf[i-1-j] = k; 
-    buf[i] = '\0';
+    int i, j;
+    static char buf[16] = {};
+    for (i = 28, j = 0; i >= 0; i-=4)
+        buf[j++] = code[(n >> i) & 0xf]; 
+    buf[j] = '\0';
     return buf;
 }
+
 int main(int argc, char *argv[])
 {
     int n, i;
     if (1 == argc) return 0;
     for (i = 1; i < argc; i++) {
         n = atoi(argv[i]);
-        //printf("n=%d\n", n);
         puts(itoa(n));
     }
     return 0;
