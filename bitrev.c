32b36ff8ce2fcab20485b0294d8a6868d3cf6b3f
harrypan <gs0622@gmail.com>
2014-08-17 11:04:11 +0800

    fix bitrev function

--- bitrev.c
+++ bitrev.c
@@ -13,12 +13,12 @@ char *itob(int n)
 
 int bitrev(int n)
 {
-    int x = (n & 0xaaaaaaaa) >> 1 | (n & 0x55555555) << 1 |
-            (n & 0xcccccccc) >> 2 | (n & 0x33333333) << 2 |
-            (n & 0xf0f0f0f0) >> 4 | (n & 0x0f0f0f0f) << 4 |
-            (n & 0xff00ff00) >> 8 | (n & 0x00ff00ff) << 8 |
-            (n >> 16) | (n << 16);
-    return x;
+    n = (n & 0xaaaaaaaa) >> 1 | (n & 0x55555555) << 1;
+    n = (n & 0xcccccccc) >> 2 | (n & 0x33333333) << 2;
+    n = (n & 0xf0f0f0f0) >> 4 | (n & 0x0f0f0f0f) << 4;
+    n = (n & 0xff00ff00) >> 8 | (n & 0x00ff00ff) << 8;
+    n = (n >> 16) | (n << 16);
+    return n;
 }
 
 int main(int argc, char *argv[])
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     for (i = 1; i < argc; i++) {
         n = atoi(argv[i]);
         printf("%s\n", itob(n));
-        printf("%x\n%s\n", bitrev(n), itob(bitrev(n)));
+        printf("%s\n", itob(bitrev(n)));
     }
     return 0;
 }
