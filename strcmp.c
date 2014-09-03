79e4d9692db84de0dc719880e4283253d7b7358f
harrypan <gs0622@gmail.com>
2014-09-03 10:51:05 +0800

    add my strcasecmp

--- strcmp.c
+++ strcmp.c
@@ -6,11 +6,20 @@ static int my_strcmp(const char *s1, const char *s2)
     while (*s1 && (*s1 == *s2)) ++s1, ++s2; /*s1 found*/
     return *s1 - *s2;
 }
+
+static int my_strcasecmp(const char *s1, const char *s2)
+{
+    while (*s1 && ((*s1 == *s2) || ((*s1 ^ 0x20) == *s2))) ++s1, ++s2;
+    return *s1 - *s2;
+}
+
 int main(int argc, char *argv[])
 {
     if (3 > argc) return 0;
-    printf("strcmp   : %d\n", strcmp(argv[1], argv[2]));
-    printf("my_strcmp: %d\n", my_strcmp(argv[1], argv[2]));
+    printf("s1=%s s2=%s\n", argv[1], argv[2]);
+    printf("strcmp       : %d\n", strcmp(argv[1], argv[2]));
+    printf("my_strcmp    : %d\n", my_strcmp(argv[1], argv[2]));
+    printf("my_strcasecmp: %d\n", my_strcasecmp(argv[1], argv[2]));
     return 0;
 }
 
