955031791baa90d34cfef6e12350700daa851de4
Harry Pan <gs0622@gmail.com>
2014-09-16 00:09:54 +0800

    draft of SortByPreDefineOrder

---
+++ SortByPreDefineOrder.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a1[] = {2, 1, 2, 5, 7, 1, 9, 3, 6, 8, 8}; 
+    int a2[] = {2, 1, 8, 3};
+    int i, j;
+    for (i = 0, j = 0; i < sizeof(a1)/sizeof(int); i++) {
+    }
+}
+
