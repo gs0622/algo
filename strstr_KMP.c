3b895bbb62d887ec9fe803e893bcac0b577cb386
harrypan <gs0622@gmail.com>
2014-09-27 01:56:48 +0800

    KMP version strstr review

--- strstr_KMP.c
+++ strstr_KMP.c
@@ -14,7 +14,7 @@ static void fail(const char *pat, int n)
 
     for (j = 1; j < n; j++) {
         i = failure[j-1];
-        while (pat[j] !=bpat[i+1] && i >= 0)
+        while (pat[j] != pat[i+1] && i >= 0)
             i = failure[i];
         if (pat[j] == pat[i+1]) failure[j]=i+1; 
         else failure[j]=-1;
@@ -41,16 +41,19 @@ int main(int argc, char *argv[])
 {
     char *s;
     if (argc<3) return 0;
+
     s = strstr(argv[1], argv[2]); /*argv[1]=string, argv[2]=pattern*/
     if (s)
         printf("1: pattern \"%s\" is found in string \"%s\" offset %ld\n", argv[2], argv[1], s-argv[1]);
     else 
         printf("1: pattern \"%s\" is not found in string \"%s\"\n", argv[2], argv[1]);
+
     s = strstr_kmp(argv[1], argv[2]); /*argv[1]=string, argv[2]=pattern*/
     if (s)
         printf("2: pattern \"%s\" is found in string \"%s\" offset %ld\n", argv[2], argv[1], s-argv[1]);
     else 
         printf("2: pattern \"%s\" is not found in string \"%s\"\n", argv[2], argv[1]);
+
     return 0;
 }
 
