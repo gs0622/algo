467674998d025cd536a06b7caf6ce9679507d614
Harry Pan <gs0622@gmail.com>
2014-09-30 01:19:23 +0800

    update dp version, O(n^2) solution

--- SumOfSubArray.c
+++ SumOfSubArray.c
@@ -13,6 +13,8 @@ int arr2[3][3] = { {1, 2, 3},
                    {4, 5, 6},
                    {7, 8, 9} };
 
+
+/*O(k^2*n^2)*/
 static void naive(int arr[], int n, int k)
 {
   int col, row;
@@ -28,33 +30,38 @@ static void naive(int arr[], int n, int k)
   }
 }
 
+/*O(n^2)*/
 static void dynamic(int arr[], int n, int k)
 {
   int col, row;
   int *dp = malloc(n*n*sizeof(int));
   assert(dp);
-  for (row = 0; row < n; row++) {
-    int sum = 0;
+  for (row = 0, dp[0] = 0; row < n; row++) {
     for (col = 0; col < n; col++) {
-      sum += arr[row*n + col];
-      if (row > 0) {
-          if (col > 0) sum += dp[(row-1)*n + col] - dp[(row-1) + col -1];
-          else sum += dp[(row-1)*n + col];
-      }
-      dp[row*n + col] = sum;
-      printf("%d%s", dp[row*n + col], (col == n-1)?"\n":",");
+      dp[row*n + col] += arr[row*n + col];
+      if (col > 0) dp[row*n + col] += dp[(row)*n + (col-1)];
+      if (row > 0) dp[row*n + col] += dp[(row-1)*n + col];
+      if (row > 0 && col > 0) dp[row*n + col] -= dp[(row-1)*n + (col-1)];
+      //printf("%d%s", dp[row*n + col], (col == n-1)?"\n":",");
     }
-    //printf("\n");
+  }
+  //printf("\n");
+  for (row = 0; row < n-k+1; row++) {
+    for (col = 0; col < n-k+1; col++) {
+      int sum = dp[(row+k-1)*n + (col+k-1)];
+      sum -= col? dp[(row+k-1)*n + (col-1)]: 0;
+      sum -= row? dp[(row-1)*n + (col+k-1)]: 0;
+      sum += (row && col)? dp[(row-1)*n + (col-1)]: 0;
+      printf("%d%s", sum, (col == n-k)?"\n":",");
+    }
   }
 }
 
 int main(void)
 {
-#if 1
-    dynamic((int *)arr1, 5, 3);
-#else
     naive((int *)arr1, 5, 3);
     naive((int *)arr2, 3, 2);
-#endif
+    dynamic((int *)arr1, 5, 3);
+    dynamic((int *)arr2, 3, 2);
 	return 0;
 }
