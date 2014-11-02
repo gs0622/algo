ae98c84f2ac8be3144fc8eb6273f79ed6c1ebbe0
harrypan <gs0622@gmail.com>
2014-11-02 15:29:06 +0800

    add help method show, and example main function

--- queue.c
+++ queue.c
@@ -2,7 +2,6 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-//#include <assert.h>
 
 struct node {
     int data;
@@ -19,18 +18,22 @@ int size(struct queue *q)
 {
     return q->size;
 }
+
 int empty(struct queue *q)
 {
     return (0 == size(q))? 1/*true*/: 0;
 }
+
 int front(struct queue *q)
 {
     return q->front->data;
 }
+
 int back(struct queue *q)
 {
     return q->back->data;
 }
+
 void push(struct queue *q, int v)
 {
     struct node *tmp = calloc(1, sizeof(struct node));
@@ -42,6 +45,7 @@ void push(struct queue *q, int v)
         q->back->next = tmp, q->back = tmp;
     q->size += 1;
 }
+
 void pop(struct queue *q)
 {
     struct node *tmp = q->front;
@@ -50,5 +54,31 @@ void pop(struct queue *q)
     else q->front = q->front->next;
     q->size -= 1;
     free(tmp);
+}
+
+void show(struct queue *q)
+{
+    int i;
+    struct node *tmp;
+    printf("size: %d: ", q->size);
+    for (i = 0, tmp = q->front; i < q->size; i++, tmp = tmp->next)
+        printf("%d%s", tmp->data, (i == q->size - 1)? "": " ");
+    if (0 == i) puts("null"); else puts("");
+}
+
+int main(void)
+{
+    struct queue q = {};
+    push(&q, 1);
+    push(&q, 7);
+    push(&q, 2);
+    show(&q);
+    printf("front: %d, back=%d, pop\n", front(&q), back(&q));
+    pop(&q);
+    printf("front: %d, back=%d, pop x2\n", front(&q), back(&q));
+    pop(&q);
+    pop(&q);
+    show(&q);
+    return 0;
 }
 
