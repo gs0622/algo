ecb24a2dd7937b744ada7f8579d263fd88358c58
harrypan <gs0622@gmail.com>
2014-12-08 13:19:42 +0800

    square root calculation by Newton's method

---
+++ SquareRoot.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float mysqrt(int val)
+{
+    int i, j;
+    float sqrt, old;
+    for (i = 1; i < val; i++) if (i*i > val) break;
+    sqrt = (float)(i);
+    for (j = 0; ; j++) {
+        sqrt = (sqrt + val / sqrt) / 2; /*Newton's method*/
+        if (old == sqrt) break; else old = sqrt;
+    }
+    return sqrt;
+}
+
+int main(int argc, char *argv[])
+{
+    int val;
+    if (2 != argc) return 0;
+    val = atoi(argv[1]);
+    if (0 >= val) return 0;
+    printf("%f\n", mysqrt(val));
+    return 0;
+}
+
