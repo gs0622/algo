0545b7dae842072d8a3064dbc55f06de211f3266
harrypan <gs0622@gmail.com>
2014-12-22 00:53:29 +0800

    grid lock 3x3 draft

---
+++ GridLock.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int adj[9][9] = {
+    { 0, 1, 0, 1, 1, 1, 0, 1, 0},
+    { 1, 0, 1, 1, 1, 1, 1, 0, 1},
+    { 0, 1, 0, 1, 1, 1, 0, 1, 0},
+    { 1, 1, 1, 0, 1, 0, 1, 1, 1},
+    { 1, 1, 1, 1, 0, 1, 1, 1, 1},
+    { 1, 1, 1, 0, 1, 0, 1, 1, 1},
+    { 0, 1, 0, 1, 1, 1, 0, 1, 0},
+    { 1, 0, 1, 1, 1, 1, 1, 0, 1},
+    { 0, 1, 0, 1, 1, 1, 0, 1, 0}
+};
+
+int visit[9];
+int pattern = 0;
+
+void dfs(int s)
+{
+    int i;
+    visit[s] = 1;
+    for (i = 0; i < 9; i++) {
+        if (adj[s][i] == 1 && !visit[i])
+            dfs(i), pattern++;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+    for (i = 0; i < 9; i++) {
+        memset(visit, 0, 9*sizeof(int));
+        dfs(i);
+    }
+    printf("%d\n", pattern);
+    return 0;
+}
