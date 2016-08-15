781f270200be7d1e10a4b70e17a3bdd1af4a9c94
Harry Pan <gs0622@gmail.com>
2016-08-15 16:45:50 +0800

    fix stupid bug during imporing data

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -2,29 +2,29 @@
 #include <fstream>
 #include <cstdlib>
 #include <cstring>
+#include <climits>
 using namespace std;
 int w[200][200]; /*Graph*/
 int d[200], visit[200], prev[200];
+
 void dijkstra(int n)
 {
-    int i, j, alt, min, a;
+    int i = n, j, min, a, b;
 
-    for (i=0; i<n; i++) {
-        min = 1000000, a=0;
+    while (i--) {
+        min = INT_MAX, a=0;
         for (j=0; j<n; j++) { /*min*/
-            if (!visit[j] && d[j] < min)
+            if (!visit[j] && d[j] < min) {
                 min = d[j], a = j;
-                //cout << i << ": " << min << ": "<< j << endl;
+            }
         }
-        if (min == 1000000) break;
-        cout << i+1 << ": " << a << ": " << min << endl;
+        if (min == INT_MAX) break;
         visit[a] = 1;
-        for (j=0;j<n;j++) {
-            if (w[a][j] == 1000000) continue; /*no edge*/
-            alt = d[a] + w[a][j];
-            if (alt < d[j]) {
-                d[j] = alt;
-                prev[j] = a; 
+        for (b=0; b<n; b++) {
+            if (w[a][b] == INT_MAX) continue; /*no edge*/
+            if (d[b] > d[a] + w[a][b]) {
+                d[b] = d[a] + w[a][b];
+                prev[b] = a; 
             }
         }
     }
@@ -39,12 +39,12 @@ int main(int argc, char **argv)
 
     for (i=0; i<200; i++)
         for (j=0; j<200; j++)
-            w[i][j] = 1000000;
+            w[i][j] = INT_MAX;
     w[0][0] = 0;
 	ifs.open(argv[2]);
 	while (ifs.good()) {
 		ifs.getline(buf, 1024);
-		token = strtok(buf, "\t\r");
+		token = strtok(buf, "\t\r ");
 		if (!token) break;
 		u = atoi(token);
 		while (token) {
@@ -54,30 +54,31 @@ int main(int argc, char **argv)
 			token = strtok(NULL, "\t ");
 			weight = atoi(token);
             w[u-1][v-1] = weight;
-			cout << u << "->" << v << ": " << weight << endl;
+			//cout << u << "->" << v << ": " << weight << endl;
 		}
 	}
     for (i=0; i<200; i++)
-        visit[i] = 0, d[i] = 1000000, prev[i] = -1;
+        visit[i] = 0, d[i] = INT_MAX, prev[i] = -1;
     d[0] = 0;
     dijkstra(n);
-    /*7,37,59,82,99,115,133,165,188,197*/
-#if 1
-    for (i=0; i<n; i++) {
-        cout << i+1 << ":" <<d[i] << " ";
-        j = i;
-        while (prev[j] != -1) {
-            cout << j+1 << " ";
-            j = prev[j];
+
+    if (argc < 4) {
+        for (i=0; i<n; i++) {
+            cout << i+1 << ":" <<d[i] << " ";
+            j = i;
+            while (prev[j] != -1) {
+                cout << j+1 << " ";
+                j = prev[j];
+            }
+            cout << endl;
         }
-        cout << endl;
+    } else { /*show these vertices only*/
+        /*7,37,59,82,99,115,133,165,188,197*/
+        cout << d[6] << ',' << d[36] << ',' << d[58] << ',';
+        cout << d[81] << ',' << d[98] << ',' << d[114] << ',';
+        cout << d[132] << ',' << d[164] << ',' << d[187] << ',';
+        cout << d[196] << endl;
     }
-#else
-    cout << d[6] << ',' << d[36] << ',' << d[58] << ',';
-    cout << d[81] << ',' << d[98] << ',' << d[114] << ',';
-    cout << d[132] << ',' << d[164] << ',' << d[187] << ',';
-    cout << d[196] << endl;
-#endif
 	ifs.close();
 	delete[] buf;
 	return 0;
