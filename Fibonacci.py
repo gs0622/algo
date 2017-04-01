87c12bac32db26165d8108ddcdfe167449a3cabb
Harry Pan <gs622@gmail.com>
2017-04-01 14:27:29 +0800

    bottom-up fibonacci api

--- Fibonacci.py
+++ Fibonacci.py
@@ -6,8 +6,23 @@ def fib(n):
     while b<n:
         print b
         a,b = b,a+b
+
+def fib2(n):
+    fib = {}
+    for k in range(1, n+1):
+        if k<=2:
+            fib[k] = 1
+        else:
+            fib[k] = fib[k-1] + fib[k-2]
+    return fib[n]
+
 def main():
     fib(100)
+    print "--"
+    print fib2(5)
+    print fib2(10)
+    print fib2(15)
+
 if __name__ == '__main__':
     main()
     
