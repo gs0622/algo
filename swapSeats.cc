dfb6c0e4bacf42529951fb2d63f3909916081894
Harry Pan <gs0622@gmail.com>
2018-07-17 23:51:59 +0800

    Rollback to vector approach rather than string.
    Signed-off-by: Harry Pan <gs0622@gmail.com>

--- swapSeats.cc
+++ swapSeats.cc
@@ -5,37 +5,46 @@ public:
 	// in: initial seats state B, target seats state A, out: steps required
 	// O(V+E) time, O(V) space: V for total vertices in terms of seats states
 	int swapSeats(vector<int>& B, vector<int>& A) {
-		string AA(A.begin(), A.end()), BB(B.begin(), B.end());	// cast int array to string
-		queue<string> q;
-		unordered_set<string> visit;				// hashset of visited state
-		q.push(BB);						// initial state
-		int res=0;
-		while (!q.empty()) {
-			int n=q.size();
-			for (int i=0; i<n; ++i) {
-				string CC=q.front(); q.pop();
-				if (CC==AA)				// match target, return require steps
-					return res;
-				visit.insert(CC);			// this state is visited
-				int empty=CC.find('0');			// locate empty seat
-				for (int j=0; j<CC.size(); ++j) {	// enumerate all the nearest neighboring states
-					if (j!=empty) {
-						swap(CC[j], CC[empty]);
-						if (visit.count(CC))	// skip visited states
-							q.push(CC);
-						swap(CC[j], CC[empty]);
-					}
-				}
-			}
-			++res;
-		}
-		return -1;
+                int cnt=0;
+                queue<vector<int>> q;
+                q.push(B);
+                while (!q.empty()) {
+                        int n=q.size();
+                        for (int i=0; i<n; ++i) {
+                                vector<int> C=q.front(); q.pop();       // C for candidate vector state
+                                if (C==A)
+                                        return cnt;
+                                int empty;                              // index of 0, aka. empty seat
+                                for (int j=0; j<C.size(); ++j) {        // O(n) to find empty seat
+                                        if (C[j]==0) {
+                                                empty=j;
+                                                break;
+                                        }       
+                                }       
+                                for (int j=0; j<C.size(); ++j)          // O(n) to enumarate all next states
+                                        if (j!=empty) {
+                                                swap(C[j],C[empty]);
+                                                q.push(C);
+                                                swap(C[j],C[empty]);
+                                        }       
+                        }               
+                        ++cnt;
+                }       
+                return -1;
 	}
 };
 int main(){
 	Solution s;
-	vector<int> before{1,2,0,3,4}, after1{1,0,2,3,4}, after2{2,4,0,1,3};
+	vector<int> before{1,2,0,3,4};
 	// e.g., 12034 has 4 neighbors 10234, 02134, 12304, 12430.
+	vector<int> after1{1,0,2,3,4};
+        vector<int> after2{1,4,2,3,0};
+        vector<int> after3{0,4,2,3,1};
+        vector<int> after4{0,2,1,3,4};
+        vector<int> after5{0,4,3,2,1};
 	cout << s.swapSeats(before, after1) << endl;
 	cout << s.swapSeats(before, after2) << endl;
+	cout << s.swapSeats(before, after3) << endl;
+	cout << s.swapSeats(before, after4) << endl;
+	cout << s.swapSeats(before, after5) << endl;
 }
